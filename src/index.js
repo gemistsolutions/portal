9ce5bc08817d1ab5bc6771dc11df04b6c986c881
Milos <milosgraovac@yahoo.com>
2025-06-02 21:02:22 -0500

    fix pls

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { AuthProvider } from './auth/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AuthProvider>
         <App />
       </AuthProvider>
