1605831374a64e776f153af287720639dcacf720
Dusan <d.graovac@yahoo.com>
2025-06-03 20:45:54 -0500

    fix

---
+++ src/Firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import {getStorage} from "firebase/storage";
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "gemist-solutions.firebaseapp.com",
+  projectId: "gemist-solutions",
+  storageBucket: "gemist-solutions.firebasestorage.app",
+  messagingSenderId: "785035861401",
+  appId: "1:785035861401:web:0aa735ab8741622d16c708",
+  measurementId: "G-9LFEXBT2MK"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
