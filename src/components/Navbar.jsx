a3cba5aecccb53191f8fbf9a80fd222e730d543c
Dusan <d.graovac@yahoo.com>
2025-06-03 20:47:30 -0500

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,6 @@ function Navbar() {
     navigate('/login')
   }
 
-  console.log(user)
   return (
     <nav className="bg-white shadow-sm">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
